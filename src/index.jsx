eaf5c35011fce37f8130a8d49d0ccc7f9c221581
nidhinagpure <nagpurenidhi8@gmail.com>
2025-03-19 17:28:06 +0530

    npm start

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+      function App(){
+        return (
+            <div>
+                <h1>hii Nidhi</h1>
+            </div>
+        )
+      }
+
+root.render(
+    <>
+    <App/>
+    </>
+)
